8dafe4c59f1c7cdefe8bace911c5252c174f701c
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2014-02-02 21:11:52 +0100

    first xml parsing test pass

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.zip "0.1.1"]]
   :main ^:skip-aot dashboard.gifs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dashboard/gifs/core.clj
+++ src/dashboard/gifs/core.clj
@@ -1,8 +1,16 @@
 (ns dashboard.gifs.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.data.zip.xml :as dzx]))
+
+(defn zip-xml [file-name]
+  (zip/xml-zip (xml/parse file-name)))
 
 (defn parse-rss [file-name]
-  [])
+  (let [zipped (zip-xml file-name)
+        items (dzx/xml-> zipped :channel :item)]
+    items))
 
 
 (defn -main
