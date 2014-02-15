d66114e1ba2538c94637bf1a71fd49d85fc7e3a9
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2014-02-15 12:44:09 +0100

    extracting item source

--- src/dashboard/gifs/core.clj
+++ src/dashboard/gifs/core.clj
@@ -17,11 +17,17 @@
   {:title     (child-text item :title)
    :image-url (extract-img-url (child-text item :description))})
 
+(defn assign-source [source item]
+  (assoc item :source source))
+
+(defn assign-source-fun [source]
+  (partial assign-source source))
+
 (defn parse-rss [file-name]
   (let [zipped (zip-xml file-name)
-        source (dzx/xml1-> zipped :channel :title)
+        source (dzx/text (dzx/xml1-> zipped :channel :title))
         items  (dzx/xml-> zipped :channel :item)]
-    (map extract-data items)))
+    (map (comp (assign-source-fun source) extract-data) items)))
 
 
 (defn -main
