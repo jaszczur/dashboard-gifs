609f3417954cd96f7e9960a3f93288247b98f082
Jaszczur <jaszczur20@interia.pl>
2014-02-22 14:00:19 +0100

    cache filename improvements & optional output dir

--- src/dashboard/gifs/cache.clj
+++ src/dashboard/gifs/cache.clj
@@ -1,10 +1,15 @@
 (ns dashboard.gifs.cache
   (:require [clojure.java.io :as io]))
 
-(defn cache-file [url]
-  (let [output-file-name (str (.hashCode url) ".img")]
-    (with-open [in  (io/input-stream url)
-                out (io/output-stream output-file-name)]
-      (io/copy in out)
-      output-file-name)))
+(defn- short-file-name [filename]
+  (str (Integer/toHexString (Math/abs (.hashCode filename))) ".img"))
+
+(defn cache-file
+  ([url] (cache-file url "/tmp"))
+  ([url output-directory]
+     (let [output-file-name (str output-directory "/" (short-file-name url))]
+       (with-open [in  (io/input-stream url)
+                   out (io/output-stream output-file-name)]
+         (io/copy in out)
+         output-file-name))))
 
