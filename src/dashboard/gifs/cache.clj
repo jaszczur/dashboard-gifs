ca7b11300b5452880b1c1bf5b07095d91875f1e9
Jaszczur <jaszczur20@interia.pl>
2014-02-22 17:13:14 +0100

    caching + main improvements

--- src/dashboard/gifs/cache.clj
+++ src/dashboard/gifs/cache.clj
@@ -7,9 +7,17 @@
 (defn cache-file
   ([url] (cache-file url "/tmp"))
   ([url output-directory]
-     (let [output-file-name (str output-directory "/" (short-file-name url))]
-       (with-open [in  (io/input-stream url)
-                   out (io/output-stream output-file-name)]
-         (io/copy in out)
-         output-file-name))))
+     (let [output-file-name (str output-directory "/" (short-file-name url))
+           output-file      (io/file output-file-name)]
+       (if-not (.exists output-file)
+         (with-open [in  (io/input-stream url)
+                     out (io/output-stream output-file)]
+           (println "Downloading file" url)
+           (io/copy in out)))
+       output-file-name)))
+
+(defn item-cached-in-dir [output-dir]
+  (fn [item]
+    (assoc item
+      :image-url (cache-file (:image-url item) output-dir))))
 
--- src/dashboard/gifs/core.clj
+++ src/dashboard/gifs/core.clj
@@ -1,15 +1,22 @@
 (ns dashboard.gifs.core
   (:gen-class)
   (:require [dashboard.gifs.parser :as parser]
-            [dashboard.gifs.output :as output]))
+            [dashboard.gifs.output :as output]
+            [dashboard.gifs.cache  :as cache]))
 
 (defn random-item [col]
   (let [r (rand-int (+ 1 (count col)))]
     (nth col r)))
 
+(def cached
+  (cache/item-cached-in-dir "/tmp"))
+
+(defn parse-files [urls]
+  (let [urls (if (empty? urls) ["http://thecodinglove.com/rss"] urls)]
+    (flatten (map #(parser/parse-rss %1) urls))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println (output/output-html
-            (random-item (parser/parse-rss (first args))))))
-
+            (cached (random-item (parse-files args))))))
